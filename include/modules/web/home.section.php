636a833b210d9ff0799cf66bfa709159663b2db1
azidzero <azid_zero@hotmail.com>
2015-04-21 16:31:53 -0500

    Revert "21-04-2015"
    This reverts commit b87a46ab3c7159827364d3140d476cc41f6ff8b8.

--- include/modules/web/home.section.php
+++ include/modules/web/home.section.php
@@ -1,71 +1,5 @@
 <!-- ##### START HOME -->
-<section class="section" id="home">
-    <div id="filter" class="col-sm-4" style="background:#2980B9;border:1px solid #CCC;padding:4px;">
-        <div>
-            <strong>Fechas</strong>
-            <div class="row">
-                <div class="col-sm-6">
-                    <img src="images/ui/door_in.png" /><input type="text" size="12" id="date_ini" class="input-date"/>
-                </div>
-                <div class="col-sm-6">
-                    <img src="images/ui/door_out.png" /><input type="text" size="12" id="date_end" class="input-date"/>
-                </div>
-            </div>
-        </div>
-        <div>            
-            <div class="dropdown">
-                <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">
-                    <img id="no_user" src="images/ui/user_default.png" /> Ocupantes
-                    <span class="caret"></span>
-                </button>
-                <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
-                    <li role="presentation"><a role="menuitem" tabindex="-1" href="javascript:void(0)"><img src="images/ui/users_1.png" /></a></li>                    
-                    <li role="presentation"><a role="menuitem" tabindex="-1" href="javascript:void(0)"><img src="images/ui/users_2.png" /></a></li>                    
-                    <li role="presentation"><a role="menuitem" tabindex="-1" href="javascript:void(0)"><img src="images/ui/users_3.png" /></a></li>                    
-                    <li role="presentation"><a role="menuitem" tabindex="-1" href="javascript:void(0)"><img src="images/ui/users_4.png" /></a></li>                    
-                    <li role="presentation"><a role="menuitem" tabindex="-1" href="javascript:void(0)"><img src="images/ui/users_5.png" /></a></li>                    
-                    <li role="presentation"><a role="menuitem" tabindex="-1" href="javascript:void(0)"><img src="images/ui/users_more.png" /></a></li>                    
-                </ul>
-            </div>
-            <div class="dropdown">
-                <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">
-                    <img id="no_user" src="images/ui/bed.png" /> Dormitorios
-                    <span class="caret"></span>
-                </button>
-                <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
-                    <li role="presentation"><a role="menuitem" tabindex="-1" href="javascript:void(0)"><img src="images/ui/users_1.png" /></a></li>                    
-                    <li role="presentation"><a role="menuitem" tabindex="-1" href="javascript:void(0)"><img src="images/ui/users_2.png" /></a></li>                    
-                    <li role="presentation"><a role="menuitem" tabindex="-1" href="javascript:void(0)"><img src="images/ui/users_3.png" /></a></li>                    
-                    <li role="presentation"><a role="menuitem" tabindex="-1" href="javascript:void(0)"><img src="images/ui/users_4.png" /></a></li>                    
-                    <li role="presentation"><a role="menuitem" tabindex="-1" href="javascript:void(0)"><img src="images/ui/users_5.png" /></a></li>                    
-                    <li role="presentation"><a role="menuitem" tabindex="-1" href="javascript:void(0)"><img src="images/ui/users_more.png" /></a></li>                    
-                </ul>
-            </div>
-            <div class="dropdown">
-                <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">
-                    <img id="no_user" src="images/ui/users_3.png" /> Capacidad
-                    <span class="caret"></span>
-                </button>
-                <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
-                    <li role="presentation"><a role="menuitem" tabindex="-1" href="javascript:void(0)"><img src="images/ui/users_1.png" /></a></li>                    
-                    <li role="presentation"><a role="menuitem" tabindex="-1" href="javascript:void(0)"><img src="images/ui/users_2.png" /></a></li>                    
-                    <li role="presentation"><a role="menuitem" tabindex="-1" href="javascript:void(0)"><img src="images/ui/users_3.png" /></a></li>                    
-                    <li role="presentation"><a role="menuitem" tabindex="-1" href="javascript:void(0)"><img src="images/ui/users_4.png" /></a></li>                    
-                    <li role="presentation"><a role="menuitem" tabindex="-1" href="javascript:void(0)"><img src="images/ui/users_5.png" /></a></li>                    
-                    <li role="presentation"><a role="menuitem" tabindex="-1" href="javascript:void(0)"><img src="images/ui/users_more.png" /></a></li>                    
-                </ul>
-                <div>
-                    <img src="images/ui/location.png" /> Ubicacion
-                    <input type="text" id="sLocate" />
-                </div>
-            </div>
-        </div>
-    </div>
-    <script>
-        $(document).ready(function () {
-            $('.input-date').datepicker({dateFormat: 'yy-mm-dd'});
-        });
-    </script>
+<section class="section" id="home">    
     <div class="container">
         <div id="sucon">
             <?php
@@ -107,8 +41,8 @@
             animation: ['kenburnsUp', 'kenburnsDown', 'kenburnsLeft', 'kenburnsRight'],
             transition: ['fade', 'zoomOut', 'swirlLeft', 'zoomIn', 'swirlRight'],
             walk: function (index, slideSettings) {
-                $('.su').css('right', '-100%').css('display', 'none');
-                $('#su_' + index).css('right', '0px').css('display', 'block');
+                $('.su').css('right', '-100%').css('display','none');
+                $('#su_' + index).css('right', '0px').css('display','block');
             }
         });
     });
